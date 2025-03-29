beaf3bace5acb8569834059d8406173983e8db36
TreesOnTop <treesontop07@gmail.com>
2025-03-29 13:07:11 -0400

    init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "com.github.treesontop"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.minestom:minestom-snapshots:f71ab6d851")
+}
---
+++ src/main/java/com/github/treesontop/Main.java
@@ -0,0 +1,7 @@
+package com.github.treesontop;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
